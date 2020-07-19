25addfdbec3bbd91736e9bafbafdd384b41064fa
HaydenLiu <huangxunliu@gmail.com>
2020-07-20 09:16:11 +1000

    Initial commit

---
+++ Assets/AAGM.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AAGM : MonoBehaviour
+{
+    public static int score = 0;
+    public Text ScoreText;
+
+    public string NextLevelName;
+
+    public int gold = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = gold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = score + "";
+
+        if(score == 0)
+        {
+            //next lv
+            SceneManager.LoadScene(NextLevelName);
+        }
+        
+    }
+}
---
+++ Assets/Pin.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Pin : MonoBehaviour
+{
+    Rigidbody2D rg;
+    public float speed = 20f;
+    bool isPined = false;
+    Spin sp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        sp = GameObject.FindGameObjectWithTag("Roundness").GetComponent<Spin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPined)
+        {
+            rg.MovePosition(rg.position + Vector2.up * speed * Time.deltaTime);
+            //AddForce会有加速度
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Roundness")
+        {
+            isPined = true;
+            this.transform.SetParent(collision.gameObject.transform);
+            AAGM.score -= 1;
+        }
+        if(collision.gameObject.tag == "Pin")
+        {
+            Camera.main.backgroundColor = Color.red;
+            StartCoroutine(Restartlv());
+            sp.speed = 50f;
+        }
+    }
+    IEnumerator Restartlv()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
